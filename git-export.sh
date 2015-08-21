ce39f17d28cf11d837e59db2d38f2d1d3a69e3a1
howard_wu <howard_wu@hiiir.com>
2015-08-21 14:14:46 +0800

    first commit

---
+++ git-export.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$1" ] || [ -z "$2" ];then
+    #echo "No  argument supplied!"
+    echo "MISSING PARAMETER: git-export [commit ID1] [command ID2] [export path]"
+    exit 1
+fi
+
+e="export"
+t="/tmp"
+
+if [ -n "$3" ];then
+    t="$3"
+fi
+
+for i in `git diff-tree --no-commit-id --name-only -r "$1" "$2"`
+    do d=`dirname "$i"`
+    f=`basename "$i"`
+    echo "$d/$f"
+    mkdir -p "$t/$e/$d"
+    cp "$d/$f" "$t/$e/$d/$f"
+    done
